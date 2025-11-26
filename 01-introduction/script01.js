c2bf2449267ddeb3e6ab721fec0e97c1c81b29c1
Yaroslav <yaroslavkovalc03@gmail.com>
2025-11-26 18:41:19 +0200

    Done lesson 1

---
+++ 01-introduction/script01.js
@@ -0,0 +1,65 @@
+//Practice
+
+// 1
+//let name = 'Max';
+//let cit = "Kyiv"
+//city = name;
+//console.log(city);
+
+// 2
+
+//let name = 'Max';
+//console.log(`hi ${1}`); // hi 1
+//console.log(`hi ${'name'}`); // hi name
+//console.log(`hi ${name}`); // hi Max
+
+// 3
+
+//let a = '5';
+//console.log(Number(a));
+//console.log(typeof Number(a));
+
+//let b = '13cvd';
+//console.log(Number.parseInt(b));
+//console.log(typeof Number(b));
+
+//let c = '12.9sxdffkg';
+//console.log(Number.parseFloat(c));
+//console.log(typeof Number(c));
+
+//let cc = '12.9sxdffkg';
+//console.log(Number.parseInt(cc));
+//console.log(typeof Number(cc));
+
+// 4
+//console.log((0.1 * 10 + 0.2 * 10) / 10);
+
+// 5
+//console.log(Math.max(20, 10, 50, 40));
+
+// 6
+//console.log(Math.random() * (4 - 2) + 2);
+
+// 7
+//const messange = 'Welcome to Kyiv! ';
+//console.log(messange.length);
+
+//8
+//const messange = 'Welcome to Kyiv! ';
+//console.log(messange.toUpperCase());
+
+//9
+//let user = {};
+//user.name = 'Max';
+//user.age = 33;
+//user.city = 'Kyiv';
+//console.log(user);
+//delete user.city;
+//user['like cars'] = true;
+//console.log(user);
+
+//10
+//for (key in user) {
+//console.log(key);
+//console.log(user[key]);
+//}
