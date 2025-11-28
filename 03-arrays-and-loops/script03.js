56a2ab7e845c3972ce7ed6752a4ce9ac2d31b297
Yaroslav <yaroslavkovalc03@gmail.com>
2025-11-28 22:28:41 +0200

    03-arrayas-and-loop

---
+++ 03-arrays-and-loops/script03.js
@@ -0,0 +1,35 @@
+//practice
+
+//while
+// let a = 0;
+// while (a < 5) {
+//   a + 1;
+//   console.log('a', a);
+// }
+
+//do
+// let a = 0;
+// do {
+//   console.log(a);
+//   a++;
+// } while (a < 5);
+
+// for (let a = 0; a < 3; a++) {
+//   console.log(a);
+// }
+
+// let a =0
+
+// let summ = 0;
+
+// while (true) {
+//   let value = +prompt('Введіть чило');
+//   if (!value) break;
+
+//   sumn += value;
+// }
+
+for (let i = 0; i < 10; i++) {
+  if (i % 2 === 0) continue;
+  console.log(i);
+}
