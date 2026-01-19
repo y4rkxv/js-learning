ed7772482cea2cedde9f438d2eee094b3c1f185b
Yaroslav <yaroslavkovalc03@gmail.com>
2026-01-20 00:02:00 +0200

    03

--- 1. practice/03-arrays-and-loops/script03.js
+++ 1. practice/03-arrays-and-loops/script03.js
@@ -1,35 +1,91 @@
-//practice
-
-//while
-// let a = 0;
-// while (a < 5) {
-//   a + 1;
-//   console.log('a', a);
-// }
-
-//do
-// let a = 0;
-// do {
-//   console.log(a);
-//   a++;
-// } while (a < 5);
+// practice
 
 // for (let a = 0; a < 3; a++) {
 //   console.log(a);
 // }
 
-// let a =0
-
 // let summ = 0;
-
 // while (true) {
-//   let value = +prompt('Введіть чило');
+//   let value = +prompt('Введіть число');
 //   if (!value) break;
+//   summ += value;
+// }
+// console.log('Summ', summ);
 
-//   sumn += value;
+// for (let i = 0; i < 10; i++) {
+//   if (i % 2 === 0) continue;
+//   console.log(i);
 // }
 
-for (let i = 0; i < 10; i++) {
-  if (i % 2 === 0) continue;
+// let arr = ['Apple', 'Orange', 'Plum'];
+
+// arr[2] = 'Lemon';
+// arr[3] = 'Cherry';
+
+// console.log(arr);
+// arr.push('Tomat');
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// let matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+// // console.log(matrix[1][2]);
+
+// let a = 5;
+// let b = a;
+// console.log(a);
+// a = 10;
+// console.log(a);
+// console.log(b);
+
+// let arr = [1, 2, 3];
+// let arr2 = [arr];
+// arr.push(4);
+// console.log(arr2);
+
+// let arr = ['a', 'b', 'c', 'd', 'f'];
+// // arr.splice(2, 1);
+// // console.log(arr);
+
+// // console.log(arr.slice(1, 3));
+// let a = [1, 2, 3];
+// console.log(arr.concat(a));
+
+// const fruts = [
+//   { id: 0, name: 'Apple' },
+//   { id: 1, name: 'Tomat' },
+//   { id: 2, name: 'Cherry' },
+//   { id: 3, name: 'Orange' },
+// ];
+
+// // console.log(fruts.find(item => item.id === 1));
+
+// // console.log(fruts.filter(item => item.id < 2));
+
+// let result = fruts.map(item => item.name.length);
+// console.log(result);
+
+// pr//
+
+// const fruts = [
+//   { id: 0, name: 'Apple' },
+//   { id: 1, name: 'Tomat' },
+//   { id: 2, name: 'Cherry' },
+//   { id: 3, name: 'Orange' },
+// ];
+
+//Виведіть парні числа від 2 до //
+for (let i = 2; i < 10; i++) {
+  if (i % 2 == 0) continue;
   console.log(i);
 }
