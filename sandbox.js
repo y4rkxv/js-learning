3e2a11d3782c7fb6f973cc0d1dd3497ece5e4e43
Yaroslav <yaroslavkovalc03@gmail.com>
2026-01-23 21:34:26 +0200

    commit

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,68 @@
-//let myName;//
-//console.log(myName);//
+// const myCity = {
+//   city: 'New York',
+//   cityGreeting: function () {
+//     console.log('Greetings!!!');
+//   },
+// };
 
-//myName = 'Yaroslav';//
-//console.log(myName);//
+// myCity.cityGreeting();
 
-//console.log(console.log(10));//
+// const person = {
+//   name: 'Bob',
+//   age: 21,
+// };
 
-//const myName = 'Yaroslav';//
-//console.log(myName);//
+// person.age = 22;
+// person.isAdult = true;
 
-const objectA = { a: 10, b: true };
-constcopy0fA = objectA;
+// console.log(person.age);
+// console.log(person.isAdult);
 
-a = 20;
+// const person = {
+//   name: 'Bob',
+//   age: 25,
+// };
+// const person2 = person;
+// person2.age = 26;
+// person2.isAdult = true;
+
+// console.log(person.age);
+
+// let a = 5;
+// let b = 3;
+// function sum(a, b) {
+//   const c = a + b;
+//   console.log(c);
+// }
+// sum(a, b);
+
+// function myFn(a, b) {
+//   let c;
+//   a = a + 1;
+//   c = a + b;
+//   return c;
+// }
+// myFn(10, 3);
+
+// function myFn() {}
+// myFn();
+
+// const personOne = {
+//   name: 'Bob',
+//   age: 21,
+// };
+
+// function increasePersonAge(person) {
+//   person.age += 1;
+//   return person;
+// }
+// increasePersonAge(personOne);
+// console.lof(personOne.age);
+
+function printMyName() {
+  console.log('Yarik');
+}
+
+console.log('Start');
+
+setTimeout(printMyName, 2000);
