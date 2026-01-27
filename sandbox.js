605628da978c3bc1cb0a94a2a17e0e6bd8fc059a
Yaroslav <yaroslavkovalc03@gmail.com>
2026-01-27 19:50:03 +0200

    commit

--- sandbox.js
+++ sandbox.js
@@ -1,68 +1,27 @@
-// const myCity = {
-//   city: 'New York',
-//   cityGreeting: function () {
-//     console.log('Greetings!!!');
-//   },
-// };
+// const myArray = ['1', '2', '3'];
+// myArray.forEach((Element, index) => {
+//   console.log(Element, index);
+// });
 
-// myCity.cityGreeting();
+// let i = 0;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 5);
 
-// const person = {
-//   name: 'Bob',
-//   age: 21,
-// };
-
-// person.age = 22;
-// person.isAdult = true;
-
-// console.log(person.age);
-// console.log(person.isAdult);
-
-// const person = {
-//   name: 'Bob',
-//   age: 25,
-// };
-// const person2 = person;
-// person2.age = 26;
-// person2.isAdult = true;
-
-// console.log(person.age);
-
-// let a = 5;
-// let b = 3;
-// function sum(a, b) {
-//   const c = a + b;
-//   console.log(c);
+const myObject = {
+  x: 10,
+  y: true,
+  z: 'abc',
+};
+// for (const key in myObject) {
+//   console.log(key, myObject[key]);
 // }
-// sum(a, b);
 
-// function myFn(a, b) {
-//   let c;
-//   a = a + 1;
-//   c = a + b;
-//   return c;
-// }
-// myFn(10, 3);
-
-// function myFn() {}
-// myFn();
-
-// const personOne = {
-//   name: 'Bob',
-//   age: 21,
-// };
-
-// function increasePersonAge(person) {
-//   person.age += 1;
-//   return person;
-// }
-// increasePersonAge(personOne);
-// console.lof(personOne.age);
-
-function printMyName() {
-  console.log('Yarik');
+// Object.keys(myObject).forEach(key => {
+//   console.log(key, myObject[key]);
+// });
+const myArray = [true, 10, 'abc', null];
+for (const key in myArray) {
+  console.log(myArray[key]);
 }
-
-console.log('Start');
-
-setTimeout(printMyName, 2000);
