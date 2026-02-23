e0db7b6726298f868a8f53a92d9bd287c1e57b3d
Yaroslav <yaroslavkovalc03@gmail.com>
2026-02-23 23:21:50 +0200

    practice

--- practice.js
+++ practice.js
@@ -267,43 +267,76 @@
 // 3) написати три варіанти студентів
 // 4) прикріпити знначення за допомогою call apply bind
 
-const student = {
-  name: 'name',
-  major: 'major',
-  gpa: 0,
-  missedClassesCount: 0,
+// const student = {
+//   name: 'name',
+//   major: 'major',
+//   gpa: 0,
+//   missedClassesCount: 0,
 
-  sayHi() {
-    alert(
-      `Student: ${this.name}, Major: ${this.major}, GPA: ${this.gpa}, Missed classes: ${this.missedClassesCount}`,
-    );
-  },
-};
-student.sayHi();
+//   sayHi() {
+//     alert(
+//       `Student: ${this.name}, Major: ${this.major}, GPA: ${this.gpa}, Missed classes: ${this.missedClassesCount}`,
+//     );
+//   },
+// };
+// student.sayHi();
 
-const student1 = {
-  name: 'Anton',
-  major: 'marketing',
-  gpa: 3,
-  missedClassesCount: 0,
-};
-const student2 = {
-  name: 'Dmytro',
-  major: 'law',
-  gpa: 3.5,
-  missedClassesCount: 7,
-};
+// const student1 = {
+//   name: 'Anton',
+//   major: 'marketing',
+//   gpa: 3,
+//   missedClassesCount: 0,
+// };
+// const student2 = {
+//   name: 'Dmytro',
+//   major: 'law',
+//   gpa: 3.5,
+//   missedClassesCount: 7,
+// };
 
-const student3 = {
-  name: 'Yaroslav',
-  major: 'economy',
-  gpa: 5,
-  missedClassesCount: 0,
-};
+// const student3 = {
+//   name: 'Yaroslav',
+//   major: 'economy',
+//   gpa: 5,
+//   missedClassesCount: 0,
+// };
 
-student.sayHi.call(student1);
+// student.sayHi.call(student1);
 
-student.sayHi.apply(student2);
+// student.sayHi.apply(student2);
 
-const yaroslavHi = student.sayHi.bind(student3);
-yaroslavHi();
+// const yaroslavHi = student.sayHi.bind(student3);
+// yaroslavHi();
+
+//------------------- freeCodeCamp
+// console.log('Hi there!');
+// console.log('I am excited to talk to you.');
+// let bot;
+// bot = 'teacherBot';
+
+// let botLocation = 'the universe';
+
+// console.log('Allow me to introduce myself.');
+
+// const botIntroduction = 'My name is ' + bot + '.';
+// console.log(botIntroduction);
+
+// const botLocationSentence = 'I live in ' + botLocation + '.';
+// console.log(botLocationSentence);
+
+// bot = 'professorBot';
+
+// const nicknameIntroduction = 'My nickname is ' + bot + '.';
+// console.log(nicknameIntroduction);
+
+// bot = 'awesomeTeacherBot';
+
+// const newNicknameGreeting =
+//   'I love my nickname but I wish people would call me ' + bot + '.';
+// console.log(newNicknameGreeting);
+
+// const favoriteSubject = 'Computer Science';
+
+// const favoriteSubjectSentence =
+//   'My favorite subject is ' + favoriteSubject + '.';
+// console.log(favoriteSubjectSentence);
