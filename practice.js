8ec73f55e7f7dbe2522239cfe6d62353132743d9
Yaroslav <yaroslavkovalc03@gmail.com>
2026-02-27 11:58:35 +0200

    practice

--- practice.js
+++ practice.js
@@ -566,3 +566,96 @@
 
 // const newSentence = `I ${repeatedLove} learning.`;
 // console.log(newSentence);
+
+//------------------------------------------------------------------------------
+// const hasDeveloperJob = true;
+
+// if (hasDeveloperJob) {
+//   console.log('Timmy is employed as a developer.');
+// }
+
+// const isTimmyAGamer = false;
+
+// if (isTimmyAGamer) {
+//   console.log('Timmy loves to play World of Warcraft.');
+// }
+
+// const timmyAge = 18;
+
+// if (timmyAge >= 16) {
+//   console.log('Timmy is old enough to drive.');
+// } else {
+//   console.log('Timmy is not old enough to drive.');
+// }
+
+//--------------------------------------------------------------------------------
+// const botName = 'MathBot';
+// const greeting = `Hi there! My name is ${botName} and I am here to teach you about the Math object!`;
+
+// console.log(greeting);
+
+// console.log(
+//   'The Math.random() method returns a pseudo random number greater than or equal to 0 and less than 1.',
+// );
+
+// const randomNum = Math.random();
+// console.log(randomNum);
+
+// console.log('Now, generate a random number between two values.');
+
+// const min = 1;
+// const max = 100;
+
+// const randomNum2 = Math.random() * (max - min) + min;
+// console.log(randomNum2);
+
+// console.log(
+//   'The Math.floor() method rounds the value down to the nearest whole integer.',
+// );
+
+// const numRoundedDown = Math.floor(6.7);
+// console.log(numRoundedDown);
+
+// console.log('Now, generate a random integer between two values.');
+
+// const randomInt = Math.floor(Math.random() * (max - min) + min);
+// console.log(randomInt);
+
+// console.log(
+//   'The Math.ceil() method rounds the value up to the nearest whole integer.',
+// );
+
+// const numRoundedUp = Math.ceil(3.2);
+// console.log(numRoundedUp);
+
+// console.log(
+//   'The Math.round() method rounds the value to the nearest whole integer.',
+// );
+
+// const numRounded = Math.round(2.7);
+// console.log(numRounded);
+// const numRounded2 = Math.round(11.2);
+// console.log(numRounded2);
+
+// console.log(
+//   'The Math.max() and Math.min() methods are used to get the maximum and minimum number from a range.',
+// );
+
+// const maxNum = Math.max(3, 125, 55, 24);
+// console.log(maxNum);
+// const minNum = Math.min(6, 90, 14, 90, 2);
+// console.log(minNum);
+
+// console.log('It was fun learning about the different Math methods with you!');
+
+//------------------------------------------------------------------------------------------
+// repeatStr(3, "a")  // має повернути "aaa"
+// repeatStr(5, "hi") // має повернути "hihihihihi"
+
+// let str = 'a';
+// let repeatStr = str.repeat(3);
+// console.log(repeatStr);
+
+// let str = 'hi';
+// let repeatStr = str.repeat(5);
+// console.log(repeatStr);
