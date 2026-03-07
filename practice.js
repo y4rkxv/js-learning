12a6a2204136cc96e7bb6327d748824368ceaa56
Yaroslav <yaroslavkovalc03@gmail.com>
2026-03-07 22:31:58 +0200

    pr

--- practice.js
+++ practice.js
@@ -901,3 +901,70 @@
 //     return names[6];
 //   }
 // }
+
+//---------------------------------------------------
+
+// const recipes = [];
+
+// const recipe1 = {
+//   name: 'Spaghetti Carbonara',
+//   ingredients: ['spaghetti', 'Parmesan cheese', 'pancetta', 'black pepper'],
+//   cookingTime: 22,
+//   totalIngredients: null,
+//   difficultyLevel: '',
+// };
+
+// const recipe2 = {
+//   name: 'Chicken Curry',
+//   ingredients: [
+//     'chicken breast',
+//     'coconut milk',
+//     'curry powder',
+//     'onion',
+//     'garlic',
+//   ],
+//   cookingTime: 42,
+//   totalIngredients: null,
+//   difficultyLevel: '',
+// };
+
+// const recipe3 = {
+//   name: 'Vegetable Stir Fry',
+//   ingredients: ['broccoli', 'carrot', 'bell pepper'],
+//   cookingTime: 15,
+//   totalIngredients: null,
+//   difficultyLevel: '',
+// };
+
+// recipes.push(recipe1, recipe2, recipe3);
+
+// function getTotalIngredients(ingredients) {
+//   return ingredients.length;
+// }
+
+// function getDifficultyLevel(cookingTime) {
+//   if (cookingTime <= 30) {
+//     return 'easy';
+//   } else if (cookingTime <= 60) {
+//     return 'medium';
+//   } else {
+//     return 'hard';
+//   }
+// }
+
+// const recipe1TotalIngredients = getTotalIngredients(recipe1.ingredients);
+// console.log(recipe1TotalIngredients);
+
+// const recipe1DifficultyLevel = getDifficultyLevel(recipe1.cookingTime);
+// console.log(recipe1DifficultyLevel);
+
+// recipe1.totalIngredients = getTotalIngredients(recipe1.ingredients);
+// recipe1.difficultyLevel = getDifficultyLevel(recipe1.cookingTime);
+
+// recipe2.totalIngredients = getTotalIngredients(recipe2.ingredients);
+// recipe2.difficultyLevel = getDifficultyLevel(recipe2.cookingTime);
+
+// recipe3.totalIngredients = getTotalIngredients(recipe3.ingredients);
+// recipe3.difficultyLevel = getDifficultyLevel(recipe3.cookingTime);
+
+// console.log(recipes);
