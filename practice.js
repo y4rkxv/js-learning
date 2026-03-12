8e0730b391eddb616613bd5a0cb97a6c587ad273
Yaroslav <yaroslavkovalc03@gmail.com>
2026-03-12 15:44:24 +0200

    pr

--- practice.js
+++ practice.js
@@ -1025,3 +1025,69 @@
 //     return `The computer's choice is wrong. The correct answer is: ${question.answer}`;
 //   }
 // }
+
+//-------------------------------------------------------------------------------------------
+// function getVowelCount(sentence) {
+//   const vowels = 'aeiou';
+//   let count = 0;
+
+//   for (const char of sentence.toLowerCase()) {
+//     if (vowels.includes(char)) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// const vowelCount = getVowelCount('Apples are tasty fruits');
+// console.log(`Vowel Count: ${vowelCount}`);
+
+// function getConsonantCount(sentence) {
+//   const consonants = 'bcdfghjklmnpqrstvwxyz';
+//   let count = 0;
+
+//   for (const char of sentence.toLowerCase()) {
+//     if (consonants.includes(char)) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// const consonantCount = getConsonantCount('Coding is fun');
+// console.log(`Consonant Count: ${consonantCount}`);
+
+// function getPunctuationCount(sentence) {
+//   const punctuations = '.,!?;:-()[]{}"\'–';
+//   let count = 0;
+
+//   for (const char of sentence) {
+//     if (punctuations.includes(char)) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// const punctuationCount = getPunctuationCount('WHAT?!?!?!?!?');
+// console.log(`Punctuation Count: ${punctuationCount}`);
+
+// function getWordCount(sentence) {
+//   if (sentence.trim() === '') {
+//     return 0;
+//   }
+
+//   const words = sentence.trim().split(' ');
+//   let count = 0;
+
+//   for (const word of words) {
+//     if (word !== '') {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+
+// const wordCount = getWordCount('I love freeCodeCamp');
+// console.log(`Word Count: ${wordCount}`);
