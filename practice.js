8c4e4e340b77c296383204cf09d39b105ffff6f0
Yaroslav <yaroslavkovalc03@gmail.com>
2026-03-25 20:17:28 +0200

    przctice

--- practice.js
+++ practice.js
@@ -1441,3 +1441,54 @@
 // function destroyer(arr, ...args) {
 //   return arr.filter(item => !args.includes(item));
 // }
+
+//  ------------------------------------------
+// function whatIsInAName(collection, source) {
+//   const sourceKeys = Object.keys(source);
+
+//   return collection.filter(obj =>
+//     sourceKeys.every(
+//       key => obj.hasOwnProperty(key) && obj[key] === source[key],
+//     ),
+//   );
+// }
+
+// const storyContainer = document.querySelector('.story-container');
+
+// const scaryStoryBtn = document.getElementById('scary-btn');
+// const funnyStoryBtn = document.getElementById('funny-btn');
+// const adventureStoryBtn = document.getElementById('adventure-btn');
+
+// const resultParagraph = document.getElementById('result');
+
+// const storyObj = {
+//   scary: {
+//     story: `In the dark woods, a group of friends stumbled upon an old, abandoned cabin. They enter the cabin and awaken something malevolent that had been dormant for centuries.`,
+//     borderColor: '#ee4b2b',
+//   },
+//   funny: {
+//     story: `During a camping trip, Mark decided to show off his culinary skills by cooking dinner over an open fire. However, his attempt caused him to burn the dinner as well as his eyebrows off.`,
+//     borderColor: '#f1be32',
+//   },
+//   adventure: {
+//     story: `Lost in the heart of the Amazon rain forest, Sarah and Jake stumbled upon an ancient temple. They braved deadly traps and encountered strange wildlife, all while deciphering cryptic clues left behind by a mysterious civilization.`,
+//     borderColor: '#acd157',
+//   },
+// };
+
+// function displayStory(genre) {
+//   if (storyObj.hasOwnProperty(genre)) {
+//     resultParagraph.textContent = storyObj[genre].story;
+//     storyContainer.style.borderColor = storyObj[genre].borderColor;
+//   }
+// }
+
+// scaryStoryBtn.addEventListener('click', () =>
+//   console.log(displayStory('scary')),
+// );
+// funnyStoryBtn.addEventListener('click', () =>
+//   console.log(displayStory('funny')),
+// );
+// adventureStoryBtn.addEventListener('click', () =>
+//   console.log(displayStory('adventure')),
+// );
